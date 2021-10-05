cbf9c0d283b6ee25caedde4ab87770521b9038e5
fakhreddineghalleb <fakhreddine.ghalleb@esprit.tn>
2021-10-05 18:12:16 +0100

    Project Completed

---
+++ app/src/main/java/tn/esprit/bugdroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package tn.esprit.bugdroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_linear)
+        setContentView(R.layout.activity_relative)
+//        setContentView(R.layout.activity_contraint)
+    }
+}
